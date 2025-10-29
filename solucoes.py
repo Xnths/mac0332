ae32ddb34a025c6597682ecbfda3a8b3eb5702ba
Xnths <jonathascastilhoes@gmail.com>
2025-10-29 20:50:09 -0300

    feat: adding solution file

---
+++ solucoes.py
@@ -0,0 +1,11 @@
+def sao_anagramas(string1, string2):
+    # TODO: Implementar lógica
+    pass
+
+def cifra_de_cesar(texto, deslocamento)
+    # TODO: Implementar lógica
+    pass
+
+def valida_cpf(cpf_string):
+    # TODO: Implementar lógica
+    pass
