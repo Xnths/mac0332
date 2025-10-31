ba5d77c5ab7047b7c64baf4d97c0ed80bbd4729f
Xnths <jonathascastilhoes@gmail.com>
2025-10-30 22:56:12 -0300

    feat: adding encontra palavra maior

--- solucoes.py
+++ solucoes.py
@@ -1,3 +1,5 @@
+import string
+
 def sao_anagramas(string1, string2):
     # TODO: Implementar lógica
     pass
@@ -5,6 +7,20 @@ def sao_anagramas(string1, string2):
 def cifra_de_cesar(texto, deslocamento):
     # TODO: Implementar lógica
     pass
+
+def encontrar_maior_palavra(frase):
+    palavras = frase.split()
+    maior_palavra = ""
+    maior_tamanho = 0
+
+    for palavra in palavras:
+        limpa = palavra.strip(string.punctuation)
+        if len(limpa) > maior_tamanho:
+            maior_tamanho = len(limpa)
+            maior_palavra = limpa
+
+    return maior_palavra
+
 
 def valida_cpf(cpf_string):
     if (cpf_string == None):
